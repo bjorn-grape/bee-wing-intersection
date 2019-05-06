2bd91d7067b2b20052e0c590006d287f955ce188
Hugo VERJUS <hugo.verjus@epita.fr>
2019-05-06 19:12:27 +0200

    up: done some cleanup

--- src/EdgeDetection.py
+++ src/EdgeDetection.py
@@ -3,57 +3,30 @@
 import ImageLoader as IL
 import cv2
 import numpy as np
-
-
-storage = IL.ImgStorage("../datas/train/")
-
-img = storage.getImgByIndex(11)
-imgLab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
-l,a,b = cv2.split(imgLab)
-
-imgBlurredL = cv2.blur(l, (11, 11))
-
-thresh0 = cv2.adaptiveThreshold(imgBlurredL, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-
-IL.showNpArrayAsImage(thresh0)
-
-
-IL.showNpArrayAsImage(imgBlurredL)
+import matplotlib.pyplot as plt
 
 
 
-kernel = np.array([[1, 1, 1],
-                   [1, -6, 1],
-                   [1, 1, 1]])
-
-dst = cv2.filter2D(imgBlurredL, -1, kernel)
-
-IL.showNpArrayAsImage(dst)
-
-
-
-ret3,th3 = cv2.threshold(dst,0,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C+cv2.THRESH_OTSU)
-
-IL.showNpArrayAsImage(th3)
-
-def tryImage(index):
-    img = storage.getImgByIndex(index)
-    imgLab = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+def detectEdges(image):
+    imgLab = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
     l,a,b = cv2.split(imgLab)
-    
     imgBlurredL = cv2.blur(l, (11, 11))
-    
-    #thresh0 = cv2.adaptiveThreshold(imgBlurredL, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-    IL.showNpArrayAsImage(thresh0)
-    
-    IL.showNpArrayAsImage(imgBlurredL)
-    
     kernel = np.array([[1, 1, 1],
-                       [1, -6, 1],
-                       [1, 1, 1]])
+                      [1, -6, 1],
+                      [1, 1, 1]])
     dst = cv2.filter2D(imgBlurredL, -1, kernel)
-    IL.showNpArrayAsImage(dst)
-    
     ret3,th3 = cv2.threshold(dst,0,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C+cv2.THRESH_OTSU)
-    IL.showNpArrayAsImage(th3)
+    return th3
+     
     
+def allPlot(): # try me
+    storage = IL.ImgStorage("../datas/train/")
+    imgNB = storage.size() / 3
+    fig, axes = plt.subplots(imgNB, 2, figsize=(8, 30))
+    for i in range(imgNB):
+        img = storage.getImgByIndex(i)
+        res = detectEdges(img)
+        axes[i, 0].imshow(img)
+        axes[i, 1].imshow(res)
+    plt.show()
+
